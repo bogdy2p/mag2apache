7abb7cdde709be9fc80d50bb75f55b96e58def5e
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-03-24 11:56:05 +0200

    Added viw/frontendlayout/ xml file

--- app/code/Pulsestorm/HelloWorldMVVM/Controller/Hello/World.php
+++ app/code/Pulsestorm/HelloWorldMVVM/Controller/Hello/World.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace Pulsestorm\HelloWorldMVVM\Controller\Hello;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
 
 class World extends \Magento\Framework\App\Action\Action
 {
 
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
     public function execute()
     {
-        echo "<p> You did it !</p>";
         var_dump(__METHOD__);
+        $page_object = $this->pageFactory->create();
+        return $page_object;
     }
 }
