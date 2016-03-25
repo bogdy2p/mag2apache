d4c0db434b8b42b5756263c9608652831eef21aa
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-03-25 12:36:01 +0200

    Wrappup of second tutorial object manager tut

--- app/code/Pulsestorm/TutorialObjectManager2/Model/Example.php
+++ app/code/Pulsestorm/TutorialObjectManager2/Model/Example.php
@@ -4,10 +4,12 @@ namespace Pulsestorm\TutorialObjectManager2\Model;
 class Example
 {
     protected $messageObject;
-	public function __construct()
+	public function __construct(Message $message)
 	{
-		$object = new Message; //
-		$this->messageObject = $object;
+            var_dump(get_class($message));
+            exit;
+//		$object = new Message; //
+		$this->messageObject = $message;
 	}
 	
     public function sendHelloAgainMessage()
